a4c28cc279a6230174af0427f9408685b7ba3ec8
juggleaddict <rocketman5421@yahoo.com>
2019-12-08 22:49:20 -0500

    init

---
+++ addressbook/schema.py
@@ -0,0 +1,35 @@
+from marshmallow import Schema, fields, pprint
+import datetime
+
+class person(object):
+    def __init__(self, firstName, lastName, email, phoneNumber, address, city, state):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.address = address
+        self.city = city
+        self.state = state
+        self.created_at = datetime.datetime.now()
+
+class PhoneNumber(fields.Field):
+    '''
+    custom field for phone numbers
+    '''
+    _serialize(self, value, attr, obj, **kwargs):
+        if value is None:
+            return ""
+        return format(int(value[:-1]), ",").replace(",", "-") + value[-1]O
+
+    _deserialize(self, value, attr, obj, **kwargs):
+        return 'TBD'
+
+class personSchema(Schema):
+    firstName = fields.Str(required=True)
+    lastName = fields.Str(required=True)
+    email = fields.Email()
+    phoneNumber = PhoneNumber()
+    address = fields.Str()
+    city = fields.Str()
+    state = fields.Str()
+    created_at = fields.DateTime()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='AddressBook',
+    version='0.1dev',
+    packages=['addressbook',],
+    license=open('LICENSE').read(),
+    long_description=open('README.txt').read(),
+)
